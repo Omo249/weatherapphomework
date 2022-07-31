e54d5013a06f76ce05123020e48d0a722b60186f
Omo249 <omolade249ahmed@gmail.com>
2022-07-31 13:48:28 +0200

    fixed h1 in js

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ function search(city) {
 
 function handleSubmit(event) {
   event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
+  let cityInputElement = document.querySelector("#email");
   search(cityInputElement.value);
 }
 
